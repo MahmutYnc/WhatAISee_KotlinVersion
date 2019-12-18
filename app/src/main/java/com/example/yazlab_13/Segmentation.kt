996080ad34ac850aecf81428581a9d7cc7e403b6
Mahmut <mahmutla33@gmail.com>
2019-12-18 11:13:31 +0300

    origin commit

---
+++ app/src/main/java/com/example/yazlab_13/Segmentation.kt
@@ -0,0 +1,12 @@
+package com.example.yazlab_13
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Segmentation : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_segmentation)
+    }
+}
